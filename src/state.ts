1cffb7857a4ea19cec9a5e711eb74eb22e62a408
Emilio Sala <emilio_k88@hotmail.com>
2026-04-07 08:24:40 +0200

    ADD: state struct for rl and commands

---
+++ src/state.ts
@@ -0,0 +1,23 @@
+import { createInterface, type Interface } from "node:readline";
+import { getCommands } from "./commands.js";
+
+export type State = {
+  rl: Interface;
+  commands: Record<string, CLICommand>;
+};
+export type CLICommand = {
+  name: string;
+  description: string;
+  callback: (state: State) => void;
+};
+
+export function initState(): State {
+  return {
+    rl: createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: "Pokedex > ",
+    }),
+    commands: getCommands(),
+  };
+}
