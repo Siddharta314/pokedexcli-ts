28439d9e2479079f25c7a420d02599f8fa26dbc3
Emilio Sala <emilio_k88@hotmail.com>
2026-04-07 08:43:07 +0200

    ADD: pokeapi class with fetch locations and types

---
+++ src/pokeapi.ts
@@ -0,0 +1,43 @@
+export class PokeAPI {
+  private static readonly baseURL = "https://pokeapi.co/api/v2";
+
+  constructor() {}
+
+  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+    try {
+      const response = await fetch(
+        pageURL ?? `${PokeAPI.baseURL}/location-area/`,
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async fetchLocation(locationName: string): Promise<Location> {
+    try {
+      const response = await fetch(
+        `${PokeAPI.baseURL}/location-area/${locationName}`,
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
+
+export type ShallowLocations = {
+  count: number;
+  next: string;
+  previous: any;
+  results: Location[];
+};
+
+export type Location = {
+  name: string;
+  url: string;
+};
