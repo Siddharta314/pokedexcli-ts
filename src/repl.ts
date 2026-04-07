a151ac8d57c508938d4a9bd3641c752be18f9a34
Emilio Sala <emilio_k88@hotmail.com>
2026-04-07 07:59:22 +0200

    ADD: startRepl and detect first word as command

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,29 @@
+import { createInterface } from "node:readline";
+import process from "node:process";
+
 export function cleanInput(input: string): string[] {
   return input
     .trim()
     .split(" ")
     .filter((w) => w.length > 0)
     .map((w) => w.toLowerCase());
+}
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Pokedex > ",
+});
+
+export function startREPL(): void {
+  rl.prompt();
+  rl.on("line", (input: string) => {
+    const cleanIn: string[] = cleanInput(input);
+    if (cleanIn.length === 0) {
+      rl.prompt();
+      return;
+    }
+    console.log(`Your command was: ${cleanIn[0]}`);
+    rl.prompt();
+  });
 }
